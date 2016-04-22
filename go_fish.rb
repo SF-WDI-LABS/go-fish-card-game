de0c96669cad7ea17658abc9ff1e285146ccd942
nathanallen <thenathanator@gmail.com>
2016-04-22 12:45:44 -0700

    add driver code

--- go_fish.rb
+++ go_fish.rb
@@ -130,13 +130,41 @@ end
 
 # Driver Code
 if __FILE__ == $0
-  # g = CardGame.new(players: 2)
-  # p g.players
+  puts "This will only print if you run `ruby go_fish.rb`"
+  # deck = CardDeck.new
+  # # # puts "cards: #{deck.cards}"
+  # # # puts "cards: #{deck}"
+  # # # puts "shuffled: #{deck.shuffle}"
+  # # puts "one drawn card: #{deck.draw_one}"
+  # # puts "two drawn cards: #{deck.draw(2)}"
 
-  deck = CardDeck.new
-  # puts "cards: #{deck.cards}"
-  puts "cards: #{deck}"
-  # puts "shuffled: #{deck.shuffle}"
-  puts "draw: #{deck.draw_one}"
-  # p deck.draw(2)
+  # cards1 = deck.draw(5)
+  # cards2 = deck.draw(5)
+  # # # puts "cards1: #{cards1.join(" ")}"
+  # # # puts "cards2: #{cards2.join(" ")}"
+
+  # h1 = HandOfCards.new(cards1)
+  # h2 = HandOfCards.new(cards2)
+
+  # puts "hand1: #{h1}"
+  # puts "hand2: #{h2}"
+
+  # p1 = CardPlayer.new(hand: h1)
+  # p2 = CardPlayer.new(hand: HandOfCards.new(deck.draw(5)) )
+
+  # puts "Hands: [ #{p1.hand} ], [ #{p2.hand} ] (before)"
+
+  # ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  # ranks.each do |rank|
+  #   print "p1, do you have any... #{rank}'s?"
+  #   if p1.hand.any?(rank: rank)
+  #     cards = p1.hand.take!(rank: rank)
+  #     print " -- YES: [ #{ cards.join(" ") } ]\n"
+  #     p2.hand.push(*cards)
+  #     break
+  #   end
+  #   print " -- NO!\n"
+  # end
+
+  # puts "Hands: [ #{p1.hand} ], [ #{p2.hand} ] (after)"
 end
