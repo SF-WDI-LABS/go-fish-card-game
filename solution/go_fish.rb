ace4a982745a67953288219a36084f27a5d8f830
nathanallen <thenathanator@gmail.com>
2017-03-02 16:11:04 -0800

    switch to array-of-strings shorthand

--- solution/go_fish.rb
+++ solution/go_fish.rb
@@ -43,8 +43,8 @@ class CardDeck
   private
 
   def generate_cards
-    ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-    suits = ["C", "D", "H", "S"]
+    ranks = %w( A 2 3 4 5 6 7 8 9 10 J Q K )
+    suits = %w( C D H S )
 
     @cards.clear
 
